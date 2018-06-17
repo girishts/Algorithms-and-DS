881ab6734f4a19a5b6e2dfcec94b981042e1f0d2
girishts <girish.ts@ge.com>
2018-06-17 12:14:16 +0530

    find subarray with given sum exists, between which indexes, and print actual sub array

--- Practice/src/com/practice/subArray/SubArray.java
+++ Practice/src/com/practice/subArray/SubArray.java
@@ -3,11 +3,36 @@ package com.practice.subArray;
 public class SubArray {
 
 	public static void main(String[] args) {
-		int arr[]= {1,2,3,4};
-		System.out.println(subArrayofSumK(arr,8));
+		int arr[]= {1,4,3,4};
+		//System.out.println(isSubArrayofSumKPresent(arr,6));
+		subArrayOfSumK(arr,6);
 	}
 	
-	public static boolean subArrayofSumK(int a[],int k) {
+	private static void subArrayOfSumK(int[] arr, int k) {
+		int l=0,m=0;
+		int sum =0;int found=0;
+		while(l<arr.length && m<arr.length) {
+			if(sum == k) {
+				System.out.println("sub array for sum "+k+" found between "+l+" and "+(m-1));
+				printSubArray(arr,l,m);found=1;
+				break;
+			}else if(sum<k) {
+				sum=sum+arr[m++];
+				
+			}else if(sum>k) {
+				sum=sum-arr[l++];
+			}
+		}
+		if(found==0)System.out.println("sub array not found");
+	}
+
+	private static void printSubArray(int[] arr, int l, int m) {
+		for (int i = l; i < m; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+	public static boolean isSubArrayofSumKPresent(int a[],int k) {
 		for(int i=0;i<a.length;i++) {
 			int sum=a[i];
 			if(sum ==k) return true;
@@ -23,4 +48,7 @@ public class SubArray {
 		
 		return false;
 	}
+	
+	
+	
 }
